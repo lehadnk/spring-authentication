3ed0402cd154a19263844afce3ae248867f7a974
Alexey Zauzin <lehadnk@gmail.com>
2023-12-23 19:42:02 +0000

    Replaced spring boot with spring context, updated gradle to 8.5

--- lib/src/main/java/authentication/jwt/dto/DecodeTokenResult.java
+++ lib/src/main/java/authentication/jwt/dto/DecodeTokenResult.java
@@ -1,7 +1,5 @@
 package authentication.jwt.dto;
 
-import org.apache.el.parser.Token;
-
 public class DecodeTokenResult<T> {
     public boolean isTokenValid;
     public boolean isTokenExpired = false;
